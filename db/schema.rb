dd0a03a0341a77708bcef4412a978947074acb29
Issei Sato <issei0813star@gmail.com>
2021-07-14 16:31:19 +0000

    microposts の表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_162632) do
+ActiveRecord::Schema.define(version: 2021_07_12_163030) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -27,4 +35,5 @@ ActiveRecord::Schema.define(version: 2021_07_10_162632) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "microposts", "users"
 end
